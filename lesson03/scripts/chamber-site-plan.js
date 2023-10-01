57d3084b515ff6899207b1f83c36060d1a295513
IamFisherman <christianperaza2000@gmail.com>
2023-09-30 20:41:25 -0400

    Site Plan finished

---
+++ lesson03/scripts/chamber-site-plan.js
@@ -0,0 +1,8 @@
+let date = new Date();
+let currentYear = date.getFullYear();
+
+document.getElementById("currentYear").innerHTML = currentYear;
+
+let lastModified = new Date(document.lastModified);
+
+document.getElementById("lastModified").innerHTML = lastModified;
