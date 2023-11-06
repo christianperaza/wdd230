c784e71600f165fb1bdf814302aa813cc3504bb8
IamFisherman <christianperaza2000@gmail.com>
2023-11-06 11:10:00 -0400

    Form home page portal created

--- scripts/confirmPassword.js
+++ scripts/confirmPassword.js
@@ -1,20 +1,21 @@
 const pass1 = document.querySelector("#pwd");
 const pass2 = document.querySelector("#pwd2");
-const message = document.querySelector("#formmessage");
+const message = document.querySelector(".formmessage");
 
 pass2.addEventListener("focusout", checkSame);
 
-// This should be refactored.
+
 function checkSame() {
 	if (pass1.value !== pass2.value) {
-		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.textContent = "⚠️ Key Phrases DO NOT MATCH!";
 		message.style.visibility = "show";
+		message.classList.toggle("show");
 		pass2.style.backgroundColor = "#fff0f3";
 		pass2.value = "";
 		pass2.focus();
 	} else {
 		message.style.display = "none";
-		pass2.style.backgroundColor = "#fff";
+		pass2.style.backgroundColor = "rgb(233, 233, 233)";
 		pass2.style.color = "#000";
 	}
 }
