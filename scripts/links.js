46fa1e54ff4f6bd8c1fa69b12e5928c4f87a346d
IamFisherman <christianperaza2000@gmail.com>
2023-11-10 11:14:31 -0400

    Home portal JSON menu finished

--- scripts/links.js
+++ scripts/links.js
@@ -2,11 +2,57 @@ const baseURL = "https://iamfisherman.github.io/wdd230/";
 
 const linksURL = "https://iamfisherman.github.io/wdd230/data/links.json";
 
-async function GetLinks()
-{
+const ul = document.querySelector("#sectionJSON");
+
+
+
+async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.table(data.lessons);
+    // console.table(data.lessons);
+
+    displayLinks(data.lessons);
 }
 
-GetLinks();
+getLinks();
+
+
+const displayLinks = (weeks) => {
+    let numberOfWeek = 1;
+
+    weeks.forEach((week) => {
+
+        
+        
+        let li = document.createElement("li");
+        let span = document.createElement("span");
+
+        span.innerHTML = `Week ${numberOfWeek++} - `;
+        li.appendChild(span);
+        
+
+        for (let index = 0; index < week.links.length; index++) {
+            let link = document.createElement("a");
+            link.setAttribute("target", "_blank");
+            link.setAttribute("href", week.links[index].url);
+            link.innerHTML = `${week.links[index].title}`;
+            
+            if (index >= 1)
+            {
+                let span2 = document.createElement("span");
+                span2.innerHTML = " | ";
+
+                li.appendChild(span2);
+            }
+            
+
+            li.appendChild(link);
+        }
+        
+
+        
+        ul.appendChild(li);
+
+    })
+
+}
