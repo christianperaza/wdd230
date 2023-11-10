d12c737f17d1ae5351794f1ef7db5939a574ea03
IamFisherman <christianperaza2000@gmail.com>
2023-11-10 18:24:55 -0400

    Chamber directory added to portal home

--- chamber/scripts/displayDirectory.js
+++ chamber/scripts/displayDirectory.js
@@ -1,3 +1,11 @@
 const baseURL = "https://iamfisherman.github.io/wdd230/";
 
 const directoryURL = "https://iamfisherman.github.io/wdd230/chamber/data/members.json";
+
+async function getCompanies() {
+    const response = await fetch(directoryURL);
+    const data = await response.json();
+    console.table(data.companies);
+}
+
+getCompanies();
