f42fe8d4070a15ab798abd02b74a6aa508c917b3
IamFisherman <christianperaza2000@gmail.com>
2023-11-10 18:49:06 -0400

    displayDirectory.js created and issues fixed

--- chamber/scripts/displayDirectory.js
+++ chamber/scripts/displayDirectory.js
@@ -2,10 +2,49 @@ const baseURL = "https://iamfisherman.github.io/wdd230/";
 
 const directoryURL = "https://iamfisherman.github.io/wdd230/chamber/data/members.json";
 
+const div = document.querySelector(".directory-container");
+
 async function getCompanies() {
     const response = await fetch(directoryURL);
     const data = await response.json();
-    console.table(data.companies);
+    // console.table(data.companies);
+
+    displayCompanies(data.companies);
 }
 
 getCompanies();
+
+const displayCompanies = (members) => {
+    members.forEach((member) => {
+        let section = document.createElement("section");
+
+        let img = document.createElement("img");
+        img.setAttribute("src", member.imageLogo);
+        img.setAttribute("alt", `${member.name}'s Logo`);
+        img.setAttribute("loading", "lazy");
+        img.setAttribute("width", "340");
+
+        let direction = document.createElement("p");
+        direction.innerHTML = `${member.address}`;
+
+        let phone = document.createElement("p");
+        phone.innerHTML = `${member.phoneNumber}`;
+
+        let mail = document.createElement("p");
+        mail.innerHTML = `${member.email}`;
+
+        let website = document.createElement("a");
+        website.setAttribute("target", "_blank");
+        website.setAttribute("href", member.websiteURL);
+        website.innerHTML = `${member.websiteURL}`;
+
+        section.appendChild(img);
+        section.appendChild(direction);
+        section.appendChild(phone);
+        section.appendChild(mail);
+        section.appendChild(website);
+
+        div.appendChild(section);
+        
+    })
+}
